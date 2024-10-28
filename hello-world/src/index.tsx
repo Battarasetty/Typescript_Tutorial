57a74e767316f54e4f40dae2c6d140893799b4b2
Battarasetty <harish93601@gmail.com>
2024-10-28 18:18:59 +0530

    type any add

--- hello-world/src/index.tsx
+++ hello-world/src/index.tsx
@@ -1,8 +1,12 @@
 //Type : number
 let sales: number = 123_456_789;
 //Type : string
-let course: string = 'Typescript';
+let course: string = "Typescript";
 //Type : boolean
 let isPublished: boolean = true;
 //Type : any
 let value;
+
+function render(document: any) {
+  console.log(document);
+}
